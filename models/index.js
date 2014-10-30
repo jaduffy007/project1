63a0f5083575a09d7e8d451ab5f7ce9f5d227095
JamesDuffy <jaduffy007@gmail.com>
2014-10-30 15:34:29 -0700

    updated index.js

--- models/index.js
+++ models/index.js
@@ -1,14 +1,28 @@
 "use strict";
-
+ 
 var fs        = require("fs");
 var path      = require("path");
 var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
-require("locus");
-
+var sequelize;
+var match;
+ 
+if (process.env.DATABASE_URL) {
+  match = process.env.DATABASE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/)
+ 
+  sequelize = new Sequelize(match[5], match[1], match[2], {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  });
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+ 
 fs
   .readdirSync(__dirname)
   .filter(function(file) {
@@ -18,52 +32,14 @@ fs
     var model = sequelize["import"](path.join(__dirname, file));
     db[model.name] = model;
   });
-
+ 
 Object.keys(db).forEach(function(modelName) {
   if ("associate" in db[modelName]) {
     db[modelName].associate(db);
   }
 });
-
-// db.User.create({
-//   username: "awesomesauce",
-//   password: "awesomesauce",
-//   email: "awesome@sauce.com"
-// });
-
-// db.User.create({
-//   username: "awesomesauce2",
-//   password: "awesomesauce2",
-//   email: "awesome2@sauce.com"
-// });
-
-// db.Post.create({
-//   title: "TESTING4",
-//   description: "THIS IS A TEST 3",
-//   UserId: 1
-// });
-
-// db.Post.create({
-//   title: "TESTING5",
-//   description: "THIS IS A TEST 4",
-//   UserId: 1
-// });
-
-// db.PostsUsers.create({
-//   PostId: 1,
-//   UserId: 2,
-//   isLiked: true
-// });
-
-// db.PostsUsers.create({
-//   PostId: 2,
-//   UserId: 2,
-//   isLiked: true
-// });
-
-
+ 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
-
-
+ 
 module.exports = db;
